eb6c5786914fa3b0d9ce335a80038bcbf434be78
Donnie Minnick <donnie.minnick@gmail.com>
2022-10-12 19:53:32 -0400

    Update to accommodate keyring

--- R/download_source_files.R
+++ R/download_source_files.R
@@ -1,22 +1,22 @@
 
 #' Download Source Files
 #'
-#' \code{download_source_files} ...
+#' \code{download_source_files} uses an ssh connection to connect to and download
+#' source files from a remote device on a wifi network.
 #'
 #' @details ...
-#' The \code{download_source_files} ...
+#' The \code{download_source_files} uses functions in the `ssh` R library.
 #'
-#' @param host_name Character, 
+#' @param host_name Character, an ssh server string in the format `[user@]hostname[:port]`.
 #'
-#' @param device_name Character, 
+#' @param device_name Character, the name of the remote device.
 #' 
-#' @param user_name Character, 
+#' @param user_name Character, the name of the user accessing the device.
 #' 
-#' @param source_path Character, 
+#' @param source_path Character, the file path to the source files to download.
 #' 
-#' @param destination_path Character, 
-#'
-#' @return ...
+#' @param destination_path Character, the file path to the destination folder
+#' to which the source files should be downloaded.
 #'
 #' @examples
 #'
@@ -24,19 +24,32 @@
 #'
 #' @export
 
-
-download_source_files <- function(host_name, 
+download_source_files <- function(host_name,
+                                  use_keyring = TRUE,
                                   device_name,
                                   user_name,
+                                  password,
                                   source_path,
                                   destination_path) {
 
         # Connect remotely to pi using an ssh connection.
         # Keyring package used to securely obtain credentials for host login.
         
-        session <- ssh::ssh_connect(host = host_name,
-                                    passwd = keyring::key_get(device_name, user_name),
-                                    verbose = FALSE)
+        if(use_keyring == TRUE) {
+            
+                session <- ssh::ssh_connect(host = host_name,
+                                            passwd = keyring::key_get(device_name, user_name),
+                                            verbose = FALSE)
+            
+        } else {
+            
+                session <- ssh::ssh_connect(host = host_name,
+                                            passwd = password,
+                                            verbose = FALSE)
+            
+        }
+    
+
         
         # Use scp protocol to download files from the host via the specified
         # directories.
